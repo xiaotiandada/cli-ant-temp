84d43c463fced1f8b2bd5ea0cf318581a8625fec
xiaotiandada <istianlei@qq.com>
2021-07-19 19:04:15 +0800

    Update app.controller.ts

--- nest-temp/src/app.controller.ts
+++ nest-temp/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HostParam, Ip, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 
 interface HelloProps {
@@ -9,16 +9,30 @@ interface HelloProps {
 }
 
 @Controller()
+// @Controller({ host: 'localhost' })
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): HelloProps {
+  getHello(@Ip() ip: string, @HostParam() hosts: string): HelloProps {
+    console.log('ip', ip);
+    console.log('hosts', hosts);
     return {
       code: 0,
       message: this.appService.getHello(),
       doc: '/openapi',
       node_env: process.env.NODE_ENV,
     };
+  }
+
+  @Get('ab*cd')
+  findAll() {
+    return 'This route uses a wildcard';
+  }
+
+  @Get('redirect')
+  @Redirect('https://docs.nestjs.com/controllers', 301)
+  redirect() {
+    return { url: 'https://docs.nestjs.com/v5/' };
   }
 }
